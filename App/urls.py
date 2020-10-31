4b0acbfa8cb3bcb494ae0ffa0f861f7b26216c1a
Anand Ranjan <44177280+Anand-ark@users.noreply.github.com>
2020-10-31 16:04:59 +0530

    Update urls.py

--- App/urls.py
+++ App/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from App import views
+from django.views.static import serve 
+from django.conf.urls import url
 urlpatterns = [
     path('', views.index, name='home-page'),
     path('diabetes', views.diabetes, name='intro'),
